15e77d9329efc9fc082a4dfdc3f004512ffe3ac6
Christian Meurin <cmmeurin@gmail.com>
2018-07-24 17:10:39 -0700

    Neck hole fix, script conv. formatting

--- scripts/doorOpenPosAngle.js
+++ scripts/doorOpenPosAngle.js
@@ -2,46 +2,46 @@
 // Licensed under GPLv3
 
 (function() {
-  var doorAngle = 70;
-  var animTime = 1;
-  var opened = false;
-  var stopped = true;
-  var doorId;
-  var curAngle = 0;
-  var yaw = 0.0; // Z rotation in Blender
-  var roll = 0.0; // Y
-  var pitch = 0.0; // X
+    var doorAngle = 70.0;
+    var animTime = 1.0;
+    var isOpened = false;
+    var isStopped = true;
+    var doorNum;
+    var curAngle = 0.0;
+    var yaw = 0.0; // Z rotation in Blender
+    var roll = 0.0; // Y
+    var pitch = 0.0; // X
 
-  function moveDoor(deltaTime) {
-    if (!stopped) {
-      var deltaAngle = (doorAngle * (deltaTime / animTime));
+    function moveDoor(deltaTime) {
+        if (!isStopped) {
+        var deltaAngle = (doorAngle * (deltaTime / animTime));
 
-      if (!opened) {
-        curAngle += deltaAngle;
-        if (curAngle >= doorAngle) curAngle = doorAngle;
-      } else {
-        curAngle -= deltaAngle;
-        if (curAngle <= 0) curAngle = 0;
-      }
+        if (!isOpened) {
+            curAngle += deltaAngle;
+            if (curAngle >= doorAngle) curAngle = doorAngle;
+        } else {
+            curAngle -= deltaAngle;
+            if (curAngle <= 0) curAngle = 0;
+        }
 
-      if (curAngle <= doorAngle && !opened || curAngle >= 0 && opened) {
-          rotation = Quat.fromPitchYawRollDegrees(pitch, yaw + curAngle, roll);
+        if (curAngle <= doorAngle && !isOpened || curAngle >= 0 && isOpened) {
+            rotation = Quat.fromPitchYawRollDegrees(pitch, yaw + curAngle, roll);
 
-          var newProps = { rotation: rotation };
-          Entities.editEntity(doorId, newProps);
-      } else {
-          opened = !opened;
-          stopped = true;
-          Script.update.disconnect(moveDoor);
-      }
+            var newProps = { rotation: rotation };
+            Entities.editEntity(doorNum, newProps);
+        } else {
+            isOpened = !isOpened;
+            isStopped = true;
+            Script.update.disconnect(moveDoor);
+        }
+        }
     }
-  }
 
-  this.clickDownOnEntity = function(entityID, mouseEvent) {
-    if (stopped) {
-        doorId = entityID;
-        stopped = false;
-        Script.update.connect(moveDoor);
+    this.clickDownOnEntity = function(entityID, mouseEvent) {
+        if (isStopped) {
+            doorNum = entityID;
+            isStopped = false;
+            Script.update.connect(moveDoor);
+        }
     }
-  }
 });
